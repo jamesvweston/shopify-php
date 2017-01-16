66fc0fa25419f9da0eb8716b232971857490b192
James Weston <james@caravana.rentals>
2017-01-16 16:26:24 -0500

    Updating ShopifyClient constructor to use ShopifyConfiguration, string, or array values

--- src/ShopifyClient.php
+++ src/ShopifyClient.php
@@ -3,11 +3,13 @@
 namespace jamesvweston\Shopify;
 
 
+use Dotenv\Dotenv;
 use jamesvweston\Shopify\Api\CarrierServiceApi;
 use jamesvweston\Shopify\Api\CollectApi;
 use jamesvweston\Shopify\Api\OrderApi;
 use jamesvweston\Shopify\Api\ProductApi;
 use jamesvweston\Shopify\Api\WebHookApi;
+use jamesvweston\Utilities\ArrayUtil AS AU;
 
 class ShopifyClient
 {
@@ -44,11 +46,38 @@ class ShopifyClient
 
     /**
      * ShopifyIntegration constructor.
-     * @param ShopifyConfiguration $shopifyConfiguration
+     * @param ShopifyConfiguration|string|array   $config
      */
-    public function __construct(ShopifyConfiguration $shopifyConfiguration)
+    public function __construct($config)
     {
-        $this->shopifyConfiguration     = $shopifyConfiguration;
+        if ($config instanceof ShopifyConfiguration)
+            $this->config               = $config;
+        else if (is_array($config))
+        {
+            $data                       = [
+                'apiKey'                => AU::get($config['apiKey']),
+                'password'              => AU::get($config['password']),
+                'hostName'              => AU::get($config['hostName']),
+            ];
+            $this->config               = new ShopifyConfiguration($data);
+        }
+        else if (is_string($config))
+        {
+            if (!is_dir($config))
+                throw new \InvalidArgumentException('A configuration must be provided');
+
+            $dotEnv                         = new Dotenv($config);
+            $dotEnv->load();
+
+            $data = [
+                'apiKey'                => getenv('SHOPIFY_API_KEY'),
+                'password'              => getenv('SHOPIFY_PASSWORD'),
+                'hostName'              => getenv('SHOPIFY_HOST_NAME'),
+            ];
+            $this->config               = new ShopifyConfiguration($data);
+        }
+        else
+            throw new \InvalidArgumentException('A configuration must be provided');
 
         $this->carrierServiceApi        = new CarrierServiceApi($this->shopifyConfiguration);
         $this->collectApi               = new CollectApi($this->shopifyConfiguration);
