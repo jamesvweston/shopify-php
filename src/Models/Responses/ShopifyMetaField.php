7d745fccdb61a637fb12503981cb568da4dabac6
James Weston <james@caravana.rentals>
2017-03-29 20:34:57 -0400

    Adding additional fields to ShopifyMetaField and no longer extending CreateShopifyMetaField

--- src/Models/Responses/ShopifyMetaField.php
+++ src/Models/Responses/ShopifyMetaField.php
@@ -3,16 +3,44 @@
 namespace jamesvweston\Shopify\Models\Responses;
 
 
-use jamesvweston\Shopify\Models\Requests\CreateShopifyMetaField;
 use jamesvweston\Utilities\ArrayUtil AS AU;
 
-class ShopifyMetaField extends CreateShopifyMetaField implements \JsonSerializable
+class ShopifyMetaField implements \JsonSerializable
 {
 
     /**
      * @var int
      */
     protected $id;
+
+    /**
+     * @var string
+     */
+    protected $namespace;
+
+    /**
+     * Identifier for the metafield (maximum of 30 characters).
+     * @var string
+     */
+    protected $key;
+
+    /**
+     * Information to be stored as metadata.
+     * @var string
+     */
+    protected $value;
+
+    /**
+     * States whether the information in the value is stored as a 'string' or 'integer.'
+     * @var string
+     */
+    protected $value_type;
+
+    /**
+     * Additional information about the metafield.
+     * @var string|null
+     */
+    protected $description;
 
     /**
      * @var int
@@ -37,8 +65,12 @@ class ShopifyMetaField extends CreateShopifyMetaField implements \JsonSerializab
 
     public function __construct($data = [])
     {
-        parent::__construct($data);
         $this->id                       = AU::get($data['id']);
+        $this->namespace                = AU::get($data['namespace']);
+        $this->key                      = AU::get($data['key']);
+        $this->value                    = AU::get($data['value']);
+        $this->value_type               = AU::get($data['value_type']);
+        $this->description              = AU::get($data['description']);
         $this->owner_id                 = AU::get($data['owner_id']);
         $this->created_at               = AU::get($data['created_at']);
         $this->updated_at               = AU::get($data['updated_at']);
@@ -50,8 +82,12 @@ class ShopifyMetaField extends CreateShopifyMetaField implements \JsonSerializab
      */
     public function jsonSerialize()
     {
-        $object                         = parent::jsonSerialize();
         $object['id']                   = $this->id;
+        $object['namespace']            = $this->namespace;
+        $object['key']                  = $this->key;
+        $object['value']                = $this->value;
+        $object['value_type']           = $this->value_type;
+        $object['description']          = $this->description;
         $object['owner_id']             = $this->owner_id;
         $object['created_at']           = $this->created_at;
         $object['updated_at']           = $this->updated_at;
@@ -74,6 +110,86 @@ class ShopifyMetaField extends CreateShopifyMetaField implements \JsonSerializab
     public function setId($id)
     {
         $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNamespace()
+    {
+        return $this->namespace;
+    }
+
+    /**
+     * @param string $namespace
+     */
+    public function setNamespace($namespace)
+    {
+        $this->namespace = $namespace;
+    }
+
+    /**
+     * @return string
+     */
+    public function getKey()
+    {
+        return $this->key;
+    }
+
+    /**
+     * @param string $key
+     */
+    public function setKey($key)
+    {
+        $this->key = $key;
+    }
+
+    /**
+     * @return string
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+
+    /**
+     * @param string $value
+     */
+    public function setValue($value)
+    {
+        $this->value = $value;
+    }
+
+    /**
+     * @return string
+     */
+    public function getValueType()
+    {
+        return $this->value_type;
+    }
+
+    /**
+     * @param string $value_type
+     */
+    public function setValueType($value_type)
+    {
+        $this->value_type = $value_type;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param null|string $description
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
     }
 
     /**
